b8ee6c31270262a9e131c9214956362d1c6aeda8
kayce <justinross28@gmail.com>
2022-11-01 17:12:39 -0700

    required variables

---
+++ routes/api-routes.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils')
+
+router.get('/notes', (_req, res) => {
+  readFromFile('./db/db.json', "utf-8").then(function (data) {
+    notesData = JSON.parse(data);
+    res.json(notesData);
+  });
+});
